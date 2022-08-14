16472b605d5ea3fc12e8aae64a3a5ffce6437bec
Jerry Jackson <jerryljackson3@gmail.com>
2022-08-14 12:30:08 -0400

    Adjusting layout for very small screens

--- src/portfolio.js
+++ src/portfolio.js
@@ -104,17 +104,23 @@ function Portfolio() {
         <h1 id="contact_title">Contact</h1>
         <p id="contact_content"><strong>Feel free to reach out below!</strong></p>
         <div id="contact_icons">
-          <a href="mailto:jerryljackson3@gmail.com">
-            <SiGmail className="icons icons_skills_contact icons_contact" title="Gmail"/>
-          </a>
+          <div>
+            <a href="mailto:jerryljackson3@gmail.com">
+              <SiGmail className="icons icons_skills_contact icons_contact" title="Gmail"/>
+            </a>
+          </div>
           <span className="icon_spacing"></span>
-          <a href="https://www.linkedin.com/in/jerry-jackson-acas-a508a5132/" target="_blank" rel="noopener noreferrer">
-            <SiLinkedin className="icons icons_skills_contact icons_contact" title="LinkedIn"/>
-          </a>
+          <div>
+            <a href="https://www.linkedin.com/in/jerry-jackson-acas-a508a5132/" target="_blank" rel="noopener noreferrer">
+              <SiLinkedin className="icons icons_skills_contact icons_contact" title="LinkedIn"/>
+            </a>
+          </div>
           <span className="icon_spacing"></span>
-          <a href="https://github.com/jerrylj2" target="_blank" rel="noopener noreferrer">
-            <FaGithub className="icons icons_skills_contact icons_contact" title="GitHub"/>
-          </a>
+          <div>
+            <a href="https://github.com/jerrylj2" target="_blank" rel="noopener noreferrer">
+              <FaGithub className="icons icons_skills_contact icons_contact" title="GitHub"/>
+            </a>
+          </div>
         </div>
       </div>
       <footer>
