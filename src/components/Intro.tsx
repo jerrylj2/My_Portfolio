4ea5f36b0d46abebbcdbd12a8721400f4ac75ca8
Jerry Jackson <jerryljackson3@gmail.com>
2023-02-02 22:30:17 -0500

    Using SWE title

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -8,7 +8,7 @@ function Intro() {
                 <img src={My_pic} alt="Me" id="my_pic"></img>
             </div>
             <div id="intro_statement">
-                <p>Hi! My name is <strong id="name">Jerry Jackson</strong> and I am a <strong>Software Developer</strong>!</p>
+                <p>Hi! My name is <strong id="name">Jerry Jackson</strong> and I am a <strong>Software Engineer</strong>!</p>
                 <p>I am passionate about the creativity in <strong>frontend</strong> development and the logical and critical thinking required for <strong>backend</strong> development.</p>
                 <p>Currently, I develop in-house software at <strong id="company">Allstate</strong>. I'm also a credentialed <strong>Actuary</strong>!</p>
             </div>
